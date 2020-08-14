830dd346743d000994e1bf75a8c513cdecd64600
Emad Alhattami <emad774373827@gmail.com>
2020-08-14 03:20:48 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -9,12 +9,53 @@ class App extends Component {
     timerIDs: []
   }
 
+  componentDidMount() {
+      this.handleAddTimer()
+    }
 
-  //Your code here:
+    render() {
+
+       return (
+         <div className="App">
+           <h1>MultiTimer</h1>
+           <button onClick={this.handleAddTimer}>Add New Timer</button>
+
+           <div className="TimerGrid">
+             {this.renderTimers()}
+           </div>
+
+         </div>
+       );
+     }
+
+    renderTimers = () => this.state.timerIDs.map(id => {
+   return <Timer key={id} id={id} removeTimer={this.removeTimer} />
+ })
+
+ handleAddTimer = () => {
+    this.setState(prevState => ({
+      timerIDs: [...prevState.timerIDs, Math.floor(Math.random()*1000)]
+    }))
+  }
+  removeTimer = id => {
+     this.setState(prevState => ({
+       timerIDs: prevState.timerIDs.filter(timer_id => timer_id !== id)
+     }))
+   }
 
 
+ }
+
+ removeTimer = id => {
+    this.setState(prevState => ({
+      timerIDs: prevState.timerIDs.filter(timer_id => timer_id !== id)
+    }))
+  }
 
 
+}
+
+export default App;
 
 
 
